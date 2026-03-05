2a0540afe6a6fad7724c4dbdffb4bda8c64838cf
madatef <madatef1@outlook.com>
2026-03-05 02:33:42 +0200

    refactor(settings): set the default redis urls to production urls instead of localhost

--- backend/app/config.py
+++ backend/app/config.py
@@ -23,9 +23,9 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
     ALGORITHM: str = "HS256"
 
-    CELERY_BROKER_URL: str = "redis://localhost:6379/0"
-    CELERY_RESULT_BACKEND: str = "redis://localhost:6379/1"
-    EVENTS_BROKER_URL: str = "redis://localhost:6379/2"
+    CELERY_BROKER_URL: str = "redis://redis:6379/0"
+    CELERY_RESULT_BACKEND: str = "redis://redis:6379/1"
+    EVENTS_BROKER_URL: str = "redis://redis:6379/2"
 
     AWS_ACCESS_KEY_ID: str
     AWS_SECRET_ACCESS_KEY: str
